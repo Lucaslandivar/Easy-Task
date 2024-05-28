1668ee9d641780195580c630d1b0e1d284575b64
Lucaslandivar <landivarlucas67@gmail.com>
2024-05-28 20:42:29 -0300

    Testador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [HeaderComponent, UserComponent],
 })
 export class AppComponent {
-  title = 'angular-iatec';
-  showFiller = false;
-  typesOfShoes: string[] = ['Home', 'Contacts', 'About'];
+  users = DUMMY_USERS;
 }
